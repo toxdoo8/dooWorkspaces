ab91c0aa06587c2ab8ec4ebf3c34a018d6e5395b
Chang Zheng <chang@cmac.lan>
2019-08-20 09:40:15 -0700

    coding bat

--- python-demo/codingBat/demo1.py
+++ python-demo/codingBat/demo1.py
@@ -1,17 +1,26 @@
 
-def lone_sum(*a):
-    c = 0
+def lone_sum(a, b, c):
+    count = len(set((a, b, c)))
+    print('count',count)
+    ct = 0
     dict = {}
-    for i in (a):
-        # print(i)
-        if i in set((a)):
-            c += 1
-            dict[i] = c
-    key = min(dict.keys(), key=(lambda k: dict[k]))
-    print(key)
-# lone_sum(1, 2, 3)
-lone_sum(9,11,11)
-# lone_sum(3, 3, 3)
+    if count == 1:
+        print('0')
+    elif count == 3:
+        total = a+b+c
+        print(total)
+    elif count == 2:
+        for i in (a, b, c):
+            # print(i)
+            if i in set((a, b, c)):
+                ct += 1
+                dict[i] = ct
+        key = min(dict.keys(), key=(lambda k: dict[k]))
+        print(key)
+lone_sum(4,5,6)
+lone_sum(3,2,3)
+lone_sum(33,22,33)
+lone_sum(3, 3, 3)
 
 # def make_bricks(small, big, goal):
 #     print(goal%5<=small and goal-(big*5)<=small)
