12ba63bf0bcef37851ce78552a7dd78204cd5bae
Chang Zheng <chang@cmac.lan>
2019-08-20 19:24:02 -0700

    update

--- leetCode/easy.py
+++ leetCode/easy.py
@@ -1,9 +1,8 @@
-def twoSum(self, nums, target):
-        dict={}
-        for k,v in enumerate(nums):
-            result=target-v
-            if result in dict:
-                return [dict[result], k]
-            dict[v]=k
-
-
+def twoSum(nums, target):
+    dict={}
+    for k,v in enumerate(nums):
+        result=target-v
+        if result in dict:
+            print([dict[result], k])
+        dict[v] = k
+twoSum([9, 0, 2, 11, 7, 15], 9)
